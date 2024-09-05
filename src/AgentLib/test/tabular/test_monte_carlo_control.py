735e6059bc5214fdedf91055bb7fe3519f2a7945
Braedan Kennedy <bkennedy@toyon.com>
2024-09-04 20:07:42 -0700

    added decay_schedule test

--- src/AgentLib/test/tabular/test_monte_carlo_control.py
+++ src/AgentLib/test/tabular/test_monte_carlo_control.py
@@ -1,9 +1,24 @@
-from ...tabular.monte_carlo_control.monte_carlo_control import *
+from ...tabular.monte_carlo_control.monte_carlo_control import decay_schedule
 
 
-def test_example():
+def test_decay_schedule():
     """
-    Example test
+    Test decay_schedule method
     """
 
-    pass
+    values = decay_schedule(
+        init_value=5, min_value=0.25, decay_ratio=0.25, max_steps=100
+    )
+
+    # Check init_value
+    assert values[0] == 5
+
+    # Check min_value
+    assert values[-1] == 0.25
+
+    # Check decay_ratio
+    assert values[23] > 0.25
+    assert values[24] == 0.25
+
+    # Check max_steps
+    assert values.size == 100
