0b1bb9a436fe9aef3216ba6d4871335cd28d6894
Braedan Kennedy <bkennedy@toyon.com>
2024-09-05 21:15:13 -0700

    double q-learning test complete

--- src/AgentLib/test/tabular/test_double_q_learning.py
+++ src/AgentLib/test/tabular/test_double_q_learning.py
@@ -7,4 +7,10 @@ def test_double_q_learning():
     Test double_q_learning method
     """
 
-    pass
+    import gymnasium as gym
+
+    from ...tabular.double_q_learning.double_q_learning import double_q_learning
+
+    env = gym.make("FrozenLake-v1")
+
+    Q, V, pi, Q_track, pi_track = double_q_learning(env)
